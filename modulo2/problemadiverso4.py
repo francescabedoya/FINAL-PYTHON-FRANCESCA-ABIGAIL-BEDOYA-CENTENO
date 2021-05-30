d5103e750e3a66dda05c324204b7b48b69b3192a
francescabedoya <85045773+francescabedoya@users.noreply.github.com>
2021-05-29 23:03:41 -0500

    Problemas Módulo 2

---
+++ modulo2/problemadiverso4.py
@@ -0,0 +1,11 @@
+print("Ingrese las notas")
+
+nota_1=float(input("Ingrese la 1era nota "))
+
+nota_2=float(input("Ingrese la 2da nota "))
+
+nota_3=float(input("Ingrese la 3era nota "))
+
+promedio = (nota_1 + nota_2 + nota_3)/3
+
+notaFinal = print("La nota final es de", promedio)
